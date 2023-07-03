6447ddd30c0255ca480309aee5e34aadf19e5096
miveh <miveh@imivechi@gmail.com>
2023-07-03 23:45:14 +0430

    arrays,arrays method, object, loops, strings

--- starter/script.js
+++ starter/script.js
@@ -160,6 +160,17 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 };
 
 console.log(yearsUntilRetirement(1998, "zaZA"));
+
+//Multiline arrow functions:
+
+let sum = (a, b) => {  // the curly brace opens a multiline function
+  let result = a + b;
+  return result; // if we use curly braces, then we need an explicit "return"
+};
+
+
+
+
 */
 
 //w03 - function calling function----------------------------------
@@ -183,7 +194,7 @@ console.log(fruitProcessor(2, 2));
 */
 
 //part2-coding challenge #1
-
+/*
 const calcAerage = (a, b, c) => (a + b + c) / 3;
 console.log(calcAerage(1, 2, 3));
 
@@ -204,3 +215,174 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 checkWinner(scoreDolphines, scoreKoalas);
 checkWinner(576, 111);
+*/
+
+//w03 - array
+//array elements cannot be accessed using arbitrary strings as indexes,
+//JavaScript arrays are zero-indexed
+//stores in memory
+//the data structure named stack.
+
+// it is copied by reference:
+
+const friend1 = "Michel";
+const friend2 = "Steven";
+const friend3 = "zahra";
+
+// 2 type for define ...
+const friends = ["Micheal", "Steven", "Zahra"]; //litteral syntax
+const years = new Array(1991, 1199, 2020);
+
+const firstName = "zaZa";
+const zahra = [firstName, 1998, 2023 - 1998, friends];
+
+//push return lenghth of Array
+const newLenght = friends.push("mivehchi");
+//add element first of array
+friends.unshift("sara");
+//remove from end
+const popedElement = friends.pop();
+friends.unshift(popedElement);
+//remove from first
+friends.shift();
+
+//return index of value..
+console.log(friends.indexOf("sara"));
+//return boolean if vaue in array or no
+console.log(friends.includes("sara"));
+
+console.log(friends);
+//Get last elements with “at”
+let fruits = ["Apple", "Orange", "Plum"];
+
+// same as fruits[fruits.length-1]
+alert(fruits.at(-1)); // Plum
+
+let fruits1 = ["Apple", "Orange", "Plum"];
+
+// iterates over array elements
+for (let fruit of fruits1) {
+  alert(fruit);
+}
+//Generally, we shouldn’t use for..in for arrays.
+let arr = ["Apple", "Orange", "Pear"];
+
+for (let key in arr) {
+  alert(arr[key]); // Apple, Orange, Pear
+}
+
+//lenght  is greatest numeric index plus one.
+let fruits12 = [];
+fruits[123] = "Apple";
+
+alert(fruits.length); // 124
+
+//Another interesting thing about the length property is that it’s writable.
+let arra = [1, 2, 3, 4, 5];
+
+arr.length = 2; // truncate to 2 elements
+alert(arr); // [1, 2]
+
+delete arra[1]; // remove "go"
+// now arr = ["I",  , "home"];
+alert(arr.length); // 3
+
+//arr.splice(start[, deleteCount, elem1, ..., elemN])
+let arr = ["I", "study", "JavaScript"];
+
+arr.splice(1, 1); // from index 1 remove 1 element
+
+alert(arr); // ["I", "JavaScript"]
+let arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 3 first elements and replace them with another
+arr.splice(0, 3, "Let's", "dance");
+
+alert(arr); // now ["Let's", "dance", "right", "now"]
+let arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 2 first elements
+let removed = arr.splice(0, 2);
+
+alert(removed); // "I", "study" <-- array of removed elements
+
+let arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+alert(arr); // "I", "study", "complex", "language", "JavaScript"
+// concat
+let arr = [1, 2];
+
+// create an array from: arr and [3,4]
+alert(arr.concat([3, 4])); // 1,2,3,4
+
+// create an array from: arr and [3,4] and [5,6]
+alert(arr.concat([3, 4], [5, 6])); // 1,2,3,4,5,6
+
+// create an array from: arr and [3,4], then add values 5 and 6
+alert(arr.concat([3, 4], 5, 6)); // 1,2,3,4,5,6
+
+// Iterate: forEach
+// The arr.forEach method allows to run a function for every element of the array.
+
+// The syntax:
+// for each element call alert
+["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
+
+//find function
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+];
+
+let user = users.find((item) => item.id == 1);
+
+alert(user.name); // John
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+  { id: 4, name: "John" },
+];
+
+// Find the index of the first John
+alert(users.findIndex((user) => user.name == "John")); // 0
+
+// Find the index of the last John
+alert(users.findLastIndex((user) => user.name == "John")); // 3
+
+//filter
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+];
+
+// returns array of the first two users
+let someUsers = users.filter((item) => item.id < 3);
+
+alert(someUsers.length); // 2
+//map
+let result = arr.map(function (item, index, array) {
+  // returns the new value instead of item
+});
+let arr = [1, 2, 15];
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+alert(arr); // 1, 2, 15
+//reduce
+let arr = [1, 2, 3, 4, 5];
+
+let result = arr.reduce((sum, current) => sum + current, 0);
+
+alert(result); // 15
+
+//
